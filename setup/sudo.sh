de72325e53e55e6ba336b74ec0e5b6b03b412550
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 <minecraft.sangelo89@gmail.com>
2022-08-16 19:30:14 +0200

    Created the sudo.sh script

---
+++ setup/sudo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (c) 2022 AstreaOS
+# /!\ Requires execution using the root account!
+
+# Print password creation message
+printf "\n\nPlease enter a password after the prompt\n\n"
+
+# Create the admin user
+sudo useradd admin -M -s /bin/bash
+
+# Give admin sudo privileges
+sudo usermod -aG sudo admin
