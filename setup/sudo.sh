a8672e339e51d66f35e550799d1cb3c8b2adf0c9
Sangelo <minecraft.sangelo89@gmail.com>
2022-08-16 19:55:35 +0200

    Update sudo.sh

--- setup/sudo.sh
+++ setup/sudo.sh
@@ -14,4 +14,4 @@ sudo usermod -aG sudo admin
 
 # Display success message
 printf "\n\n(i) Script finished.\n(i) You can now log into the admin account using \"su admin\""
-printf "\nIf you want to give your own user sudo rights, log into the admin account and run \"sudo usermod -aG sudo <username>\n\n\""
+printf "\n    If you want to give your own user sudo rights, log into the admin account and run \"sudo usermod -aG sudo <username>\"\n\n"
